7ee0c57f1e0c3ec30c9c5aa7b385e5c91abd3823
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-03-14 20:51:46 +0300

    Add files via upload

---
+++ NET.S.2018.Haiduk.01/MethodsOfSorting.Tests/SortingClassTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MethodsOfSorting.Tests
+{
+    [TestClass]
+    public class SortingClassTests
+    {
+        [TestMethod]
+        public void MergeSortingTest1()
+        {
+            // Arrange
+            int[] unsorted = { 1, -5, 0, 15, -4, 27 };
+
+            // Act
+            int[] sortedByMergeSort = SortingClass.MergeSorting(unsorted);
+
+            // Assert
+            CollectionAssert.AreEqual(new int[] { -5, -4, 0, 1, 15, 27 }, sortedByMergeSort);
+        }
+
+        [TestMethod]
+        public void MergeSortingTest2()
+        {
+            // Arrange
+            int[] unsorted = { 1 };
+
+            // Act
+            int[] sortedByMergeSort = SortingClass.MergeSorting(unsorted);
+
+            // Assert
+            CollectionAssert.AreEqual(new int[] { 1 }, sortedByMergeSort);
+        }
+
+        [TestMethod]
+        public void QuickSortingTest1()
+        {
+            // Arrange
+            int[] unsorted = { 1, -5, 0, 15, -4, 27 };
+
+            // Act
+            int[] sortedByQuickSort = SortingClass.QuickSorting(unsorted, 0, unsorted.Length - 1);
+
+            // Assert
+            CollectionAssert.AreEqual(new int[] { -5, -4, 0, 1, 15, 27 }, sortedByQuickSort);
+        }
+
+        [TestMethod]
+        public void QuickSortingTest2()
+        {
+            // Arrange
+            int[] unsorted = { 0 };
+
+            // Act
+            int[] sortedByQuickSort = SortingClass.QuickSorting(unsorted, 0, unsorted.Length - 1);
+
+            // Assert
+            CollectionAssert.AreEqual(new int[] { 0 }, sortedByQuickSort);
+        }
+    }
+}
---
+++ NET.S.2018.Haiduk.01/MethodsOfSorting/SortingClass.cs
@@ -0,0 +1,124 @@
+﻿namespace MethodsOfSorting
+{
+    /// <summary>
+    /// Class that contains methods of Quick Sorting and Merge Sorting of integer arrays
+    /// </summary>
+    public class SortingClass
+    {
+        /// <summary>
+        /// This method divides unsorted array in half and sends them to sorting method
+        /// </summary>
+        /// <param name="arr">initial unsorted array</param>
+        /// <returns>sorted by merging array</returns>
+        public static int[] MergeSorting(int[] arr)
+        {
+            if (arr.Length == 1)
+            {
+                return arr;
+            }
+
+            int arrCenter = arr.Length / 2;
+            int[] arr1 = new int[arrCenter];
+            int[] arr2 = new int[arr.Length - arrCenter];
+            for (int i = 0; i < arrCenter; i++)
+            {
+                arr1[i] = arr[i];
+            }
+
+            for (int i = arrCenter; i < arr.Length; i++)
+            {
+                arr2[i - arrCenter] = arr[i];
+            }
+
+            return Merging(MergeSorting(arr1), MergeSorting(arr2));
+        }
+
+        /// <summary>
+        /// This method sorts 2 accepted arrays by merging
+        /// </summary>
+        /// <param name="arr1">1st accepted array</param>
+        /// <param name="arr2">2nd accepted array</param>
+        /// <returns>sorted by merging array</returns>
+        public static int[] Merging(int[] arr1, int[] arr2)
+        {
+            int a = 0, b = 0;
+            int[] merged = new int[arr1.Length + arr2.Length];
+            for (int i = 0; i < arr1.Length + arr2.Length; i++)
+            {
+                if (b < arr2.Length && a < arr1.Length)
+                {
+                    if (arr1[a] > arr2[b])
+                    {
+                        merged[i] = arr2[b++];
+                    }
+                    else
+                    {
+                        merged[i] = arr1[a++];
+                    }
+                }
+                else
+                  if (b < arr2.Length)
+                {
+                    merged[i] = arr2[b++];
+                }
+                else
+                {
+                    merged[i] = arr1[a++];
+                }
+            }
+
+            return merged;
+        }
+
+        /// <summary>
+        /// This method sorts accepted array by Quick sorting
+        /// </summary>
+        /// <param name="arr">initial unsorted array</param>
+        /// <param name="start">index of first element of accepted array</param>
+        /// <param name="end">index of last element of accepted array</param>
+        /// <returns>sorted array by Quick sorting</returns>
+        public static int[] QuickSorting(int[] arr, int start, int end)
+        {
+            if (arr.Length == 1)
+            {
+                return arr;
+            }
+            
+            int p = arr[((end - start) / 2) + start];
+            int i = start, j = end;
+            while (i <= j)
+            {
+                while (arr[i] < p && i <= end)
+                {
+                    ++i;
+                }
+
+                while (arr[j] > p && j >= start)
+                {
+                    --j;
+                }
+
+                if (i <= j)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    ++i;
+                    --j;
+                }
+            }
+
+            if (j > start)
+            {
+                QuickSorting(arr, start, j);
+            }
+
+            if (i < end)
+            {
+                QuickSorting(arr, i, end);
+            }
+
+            return arr;
+        }
+    }
+}
