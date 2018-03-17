4c95687eb210587eb428e2b7c33aa2b932de9c31
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-03-17 18:27:57 +0300

    Update SortingClass.cs

--- NET.S.2018.Haiduk.01/MethodsOfSorting/SortingClass.cs
+++ NET.S.2018.Haiduk.01/MethodsOfSorting/SortingClass.cs
@@ -1,108 +1,56 @@
-﻿namespace MethodsOfSorting
+using System;
+
+namespace MethodsOfSorting
 {
     /// <summary>
     /// Class that contains methods of Quick Sorting and Merge Sorting of integer arrays
     /// </summary>
     public class SortingClass
     {
-        /// <summary>
-        /// This method divides unsorted array in half and sends them to sorting method
-        /// </summary>
-        /// <param name="arr">initial unsorted array</param>
-        /// <returns>sorted by merging array</returns>
-        public static int[] MergeSorting(int[] arr)
-        {
-            if (arr.Length == 1)
-            {
-                return arr;
-            }
-
-            int arrCenter = arr.Length / 2;
-            int[] arr1 = new int[arrCenter];
-            int[] arr2 = new int[arr.Length - arrCenter];
-            for (int i = 0; i < arrCenter; i++)
-            {
-                arr1[i] = arr[i];
-            }
-
-            for (int i = arrCenter; i < arr.Length; i++)
-            {
-                arr2[i - arrCenter] = arr[i];
-            }
-
-            return Merging(MergeSorting(arr1), MergeSorting(arr2));
-        }
-
-        /// <summary>
-        /// This method sorts 2 accepted arrays by merging
-        /// </summary>
-        /// <param name="arr1">1st accepted array</param>
-        /// <param name="arr2">2nd accepted array</param>
-        /// <returns>sorted by merging array</returns>
-        public static int[] Merging(int[] arr1, int[] arr2)
-        {
-            int a = 0, b = 0;
-            int[] merged = new int[arr1.Length + arr2.Length];
-            for (int i = 0; i < arr1.Length + arr2.Length; i++)
-            {
-                if (b < arr2.Length && a < arr1.Length)
-                {
-                    if (arr1[a] > arr2[b])
-                    {
-                        merged[i] = arr2[b++];
-                    }
-                    else
-                    {
-                        merged[i] = arr1[a++];
-                    }
-                }
-                else
-                  if (b < arr2.Length)
-                {
-                    merged[i] = arr2[b++];
-                }
-                else
-                {
-                    merged[i] = arr1[a++];
-                }
-            }
-
-            return merged;
-        }
-
+        #region Public methods
         /// <summary>
         /// This method sorts accepted array by Quick sorting
         /// </summary>
-        /// <param name="arr">initial unsorted array</param>
+        /// <param name="array">initial unsorted array</param>
         /// <param name="start">index of first element of accepted array</param>
         /// <param name="end">index of last element of accepted array</param>
         /// <returns>sorted array by Quick sorting</returns>
-        public static int[] QuickSorting(int[] arr, int start, int end)
+        public static void QuickSorting(int[] array, int start, int end)
         {
-            if (arr.Length == 1)
+            if (array.Length == 0)
             {
-                return arr;
+                throw new ArgumentException($"{nameof(array)} length is 0.");
             }
-            
-            int p = arr[((end - start) / 2) + start];
+
+            if (array == null)
+            {
+                throw new NullReferenceException($"{nameof(array)} is null.");
+            }
+
+            if (array.Length == 1)
+            {
+                return;
+            }
+
+            int center = array[((end - start) / 2) + start];
             int i = start, j = end;
             while (i <= j)
             {
-                while (arr[i] < p && i <= end)
+                while (array[i] < center && i <= end)
                 {
                     ++i;
                 }
 
-                while (arr[j] > p && j >= start)
+                while (array[j] > center && j >= start)
                 {
                     --j;
                 }
 
                 if (i <= j)
                 {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
                     ++i;
                     --j;
                 }
@@ -110,15 +58,92 @@
 
             if (j > start)
             {
-                QuickSorting(arr, start, j);
+                QuickSorting(array, start, j);
             }
 
             if (i < end)
             {
-                QuickSorting(arr, i, end);
+                QuickSorting(array, i, end);
+            }
+        }
+
+        /// <summary>
+        /// This method divides unsorted array in half and sends them to sorting method
+        /// </summary>
+        /// <param name="array">initial unsorted array</param>
+        /// <returns>sorted by merging array</returns>
+        public static int[] MergeSorting(int[] array)
+        {
+            if (array.Length == 0)
+            {
+                throw new ArgumentException($"{nameof(array)} length is 0.");
             }
 
-            return arr;
+            if (array == null)
+            {
+                throw new NullReferenceException($"{nameof(array)} is null.");
+            }
+
+            if (array.Length == 1)
+            {
+                return array;
+            }
+
+            int arrCenter = array.Length / 2;
+            int[] arr1 = new int[arrCenter];
+            int[] arr2 = new int[array.Length - arrCenter];
+            for (int i = 0; i < arrCenter; i++)
+            {
+                arr1[i] = array[i];
+            }
+
+            for (int i = arrCenter; i < array.Length; i++)
+            {
+                arr2[i - arrCenter] = array[i];
+            }
+
+            return Merging(MergeSorting(arr1), MergeSorting(arr2));
         }
+
+        #endregion
+
+        #region Private methods
+        /// <summary>
+        /// This method sorts 2 accepted arrays by merging
+        /// </summary>
+        /// <param name="array1">1st accepted array</param>
+        /// <param name="array2">2nd accepted array</param>
+        /// <returns>sorted by merging array</returns>
+        private static int[] Merging(int[] array1, int[] array2)
+        {
+            int a = 0, b = 0;
+            int[] merged = new int[array1.Length + array2.Length];
+            for (int i = 0; i < array1.Length + array2.Length; i++)
+            {
+                if (b < array2.Length && a < array1.Length)
+                {
+                    if (array1[a] > array2[b])
+                    {
+                        merged[i] = array2[b++];
+                    }
+                    else
+                    {
+                        merged[i] = array1[a++];
+                    }
+                }
+                else
+                  if (b < array2.Length)
+                {
+                    merged[i] = array2[b++];
+                }
+                else
+                {
+                    merged[i] = array1[a++];
+                }
+            }
+
+            return merged;
+        }
+        #endregion                
     }
 }
