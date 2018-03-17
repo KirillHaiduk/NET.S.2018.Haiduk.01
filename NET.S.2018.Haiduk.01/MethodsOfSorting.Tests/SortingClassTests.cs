6ab7ced6f64a9171d43094ff4b4d64468d5adcc6
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-03-17 18:28:39 +0300

    Update SortingClassTests.cs

--- NET.S.2018.Haiduk.01/MethodsOfSorting.Tests/SortingClassTests.cs
+++ NET.S.2018.Haiduk.01/MethodsOfSorting.Tests/SortingClassTests.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MethodsOfSorting.Tests
 {
+    #region MergeSortTests
+
     [TestClass]
     public class SortingClassTests
     {
+        [TestMethod]
+        public void MergeSortingTest_AcceptsNullArray_ThrowsNullReferenceException()
+        {
+            int[] unsorted = null;
+            Assert.ThrowsException<NullReferenceException>(() => SortingClass.MergeSorting(unsorted));
+        }
+
+        [TestMethod]
+        public void MergeSortingTest_AcceptsEmptyArray_ThrowsArgumentException()
+        {
+            int[] unsorted = new int[0];
+            Assert.ThrowsException<ArgumentException>(() => SortingClass.MergeSorting(unsorted));
+        }
+
         [TestMethod]
         public void MergeSortingTest1()
         {
@@ -32,29 +49,109 @@ namespace MethodsOfSorting.Tests
         }
 
         [TestMethod]
+        public void MergeSortingTest3()
+        {
+            // Arrange
+            int[] unsorted = new int[100];
+            Random random = new Random();
+            for (int i = 0; i < unsorted.Length; ++i)
+            {
+                unsorted[i] = random.Next(int.MinValue, int.MaxValue);
+            }
+
+            // Act
+            int[] sortedByMergeSort = SortingClass.MergeSorting(unsorted);
+
+            // Assert
+            Assert.IsTrue(IsArraySorted(sortedByMergeSort));
+        }
+        #endregion
+
+        #region QuickSortTests
+
+        [TestMethod]
         public void QuickSortingTest1()
         {
             // Arrange
-            int[] unsorted = { 1, -5, 0, 15, -4, 27 };
+            int[] unsorted1 = { 1, -5, 0, 15, -4, 27 };
+            int[] unsorted2 = { 1, -5, 0, 15, -4, 27 };
 
             // Act
-            int[] sortedByQuickSort = SortingClass.QuickSorting(unsorted, 0, unsorted.Length - 1);
-
+            Array.Sort(unsorted2);
+            SortingClass.QuickSorting(unsorted1, 0, unsorted1.Length - 1);
+            
             // Assert
-            CollectionAssert.AreEqual(new int[] { -5, -4, 0, 1, 15, 27 }, sortedByQuickSort);
+            CollectionAssert.AreEqual(unsorted2, unsorted1);
         }
 
         [TestMethod]
         public void QuickSortingTest2()
         {
             // Arrange
-            int[] unsorted = { 0 };
+            int[] unsorted = new int[100];
+            Random random = new Random();
+            for (int i = 0; i < unsorted.Length; ++i)
+            {
+                unsorted[i] = random.Next(1, 10);
+            }
 
             // Act
-            int[] sortedByQuickSort = SortingClass.QuickSorting(unsorted, 0, unsorted.Length - 1);
+            SortingClass.QuickSorting(unsorted, 0, unsorted.Length - 1);
 
             // Assert
-            CollectionAssert.AreEqual(new int[] { 0 }, sortedByQuickSort);
+            Assert.IsTrue(IsArraySorted(unsorted));                   
         }
+
+        [TestMethod]
+        public void QuickSortingTest3()
+        {
+            // Arrange
+            int[] unsorted = new int[100];
+            Random random = new Random();
+            for (int i = 0; i < unsorted.Length; ++i)
+            {
+                unsorted[i] = random.Next(int.MinValue, int.MaxValue);
+            }
+
+            // Act
+            SortingClass.QuickSorting(unsorted, 0, unsorted.Length - 1);
+
+            // Assert
+            Assert.IsTrue(IsArraySorted(unsorted));
+        }
+
+        [TestMethod]        
+        public void QuickSortingTest_AcceptsNullArray_ThrowsNullReferenceException()
+        {            
+            int[] unsorted = null;
+            Assert.ThrowsException<NullReferenceException>(() => SortingClass.QuickSorting(unsorted, 0, unsorted.Length - 1));
+        }
+
+        [TestMethod]
+        public void QuickSortingTest_AcceptsEmptyArray_ThrowsArgumentException()
+        {
+            int[] unsorted = new int[0];
+            Assert.ThrowsException<ArgumentException>(() => SortingClass.QuickSorting(unsorted, 0, unsorted.Length - 1));
+        }
+
+        private static bool IsArraySorted(int[] array)
+        {
+            int a = 0;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] <= array[i + 1])
+                {
+                    a++;
+                }
+            }
+
+            if (a == array.Length - 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        #endregion
     }
 }
